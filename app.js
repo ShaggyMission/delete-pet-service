136be3ad6ac4a127fdd92af8b026d96b029593c0
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-06-29 11:07:19 -0500

    feat: enable CORS in deletePets service to allow cross-origin requests

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors'); 
 
 const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
+app.use(cors()); 
 app.use('/deletePets-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
